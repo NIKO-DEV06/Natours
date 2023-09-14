3dc0b759e7ebc6e04235fe797240341cb4c95ea7
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-09-14 20:33:50 +0100

    learnt about mongoDb aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,7 @@ const APIFeatures = require("../utils/apiFeatures");
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
   req.query.sort = "price";
-  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  req.query.fields = "name,price,ratingAverage,summary,difficulty";
   next();
 };
 
@@ -176,6 +176,47 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: "success",
       data: null,
+    });
+  } catch (err) {
+    res.status(204).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          num: { $sum: 1 },
+          numRatings: { $sum: "$ratingQuantity" },
+          avgRating: { $avg: "$ratingAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+      // {
+      //   $match: { _id: { $ne: "EASY" } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
     });
   } catch (err) {
     res.status(204).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,6 +6,7 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
   // checkID,
   // checkBody,
 } = require("../controllers/tourController");
@@ -15,6 +16,7 @@ const router = express.Router();
 // router.param("id", checkID);
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+router.route("/tour-stats").get(getTourStats);
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
