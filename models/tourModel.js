ba98e5b6f21729ce8874a8e09fba22114f0553d9
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-09-18 21:42:25 +0100

    learnt about mongoose validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxlength: [40, "A tour name must have less or equal than 40 characters"],
+      minlength: [10, "A tour name must have less or equal than 40 characters"],
+      validate: [validator.isAlpha, "Tour name must only conatin characters"],
     },
     slug: String,
     duration: {
@@ -21,11 +25,29 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult",
+      },
     },
-    ratingAverage: { type: Number, default: 4.5 },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be above 5.0"],
+    },
     ratingQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, "A tour must have a name"] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: "Discount Price ({VALUE}) must be below the regular price",
+      },
+    },
     summary: {
       type: String,
       trim: true,
