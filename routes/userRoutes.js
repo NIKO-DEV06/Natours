33fb479fa3eb03b277bdcaceb9c82888af1df48f
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-09-24 17:12:41 +0100

    created mongoose user model and started working on the authController

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,12 @@ const {
   updateUser,
   deleteUser,
 } = require("../controllers/userController");
+const { signup } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route("").get(getAllUsers).post(createUser);
+router.post("/signup", signup);
+router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
