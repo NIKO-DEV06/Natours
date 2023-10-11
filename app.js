edb042c4548e37e4c6ca6a5236d195a662ef987c
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-10-11 23:46:29 +0100

    implemented data sanitization to protect against nosql query injections and XSS

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,14 +12,40 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// SECURITY HTTP HEADERS
+app.use(helmet());
+
+// DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+// LIMIT REQUETS FROM SAME API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+// SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
+// TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
