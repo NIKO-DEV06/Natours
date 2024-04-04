f43f5d2474c50109b6df650b8a1b0e30a853e52d
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2024-04-04 19:08:28 +0100

    implemented stripe webhooks

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
+const bookingController = require("./controllers/bookingController");
+
 const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
@@ -67,6 +69,12 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again in an hour!",
 });
 app.use("/api", limiter);
+
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  bookingController.webhookCheckout,
+);
 
 // BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
 app.use(
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,11 +8,16 @@ const {
   getMyTours,
 } = require("../controllers/viewsController");
 const { isLoggedIn, protect } = require("../controllers/authController");
-const { createBookingCheckout } = require("../controllers/bookingController");
+// const { createBookingCheckout } = require("../controllers/bookingController");
 
 const router = express.Router();
 
-router.get("/", createBookingCheckout, isLoggedIn, getOverview);
+router.get(
+  "/",
+  // createBookingCheckout,
+  isLoggedIn,
+  getOverview,
+);
 router.get("/tour/:slug", isLoggedIn, getTour);
 router.get("/login", isLoggedIn, getLoginForm);
 router.get("/me", protect, getAccount);
